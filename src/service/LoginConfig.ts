247bb1d90779ef7460e5341c342e390a82cc306a
asikpradhan@gmail.com <tsb-3DS-ngG-J9K>
2020-11-19 11:13:16 +0545

    feat: init

--- src/service/LoginConfig.ts
+++ src/service/LoginConfig.ts
@@ -1,15 +1,14 @@
-const OKTA_DOMAIN = 'auth.contentmunch.com';
 const CLIENT_ID = '0oakabbaYBaBrYgFf5d5';
 export const CALLBACK_PATH = '/implicit/callback';
-
-const ISSUER = `https://${OKTA_DOMAIN}/oauth2/default`;
+const ISSUER = `https://auth.contentmunch.com/oauth2/default`;
 const HOST = window.location.host;
-const REDIRECT_URI = `http://${HOST}${CALLBACK_PATH}`;
+const REDIRECT_URI = `https://${HOST}${CALLBACK_PATH}`;
+const DEV_REDIRECT_URI = `http://${HOST}${CALLBACK_PATH}`;
 
 export const config = {
     issuer: ISSUER,
     clientId: CLIENT_ID,
-    redirectUri: REDIRECT_URI,
+    redirectUri: process.env.NODE_ENV && process.env.NODE_ENV === "development" ? DEV_REDIRECT_URI : REDIRECT_URI,
     scopes: ['openid', 'profile', 'email'],
     pkce: true
 };
